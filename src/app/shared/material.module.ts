c381da0b1b03e319c963497e75cf2665b94db723
Piotr Zysk <pizy123@gmail.com>
2019-07-09 22:31:02 +0200

    Fix issues related to incompatible versions of dependencies.

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 @NgModule({
-  declarations: [],
   imports: [
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
+  ],
+  exports: [
+    BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule
   ]
