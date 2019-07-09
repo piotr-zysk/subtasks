95bd94aceb7052158f962920da4daa58a8f3980c
Piotr Zysk <pizy123@gmail.com>
2019-07-09 21:52:59 +0200

    Angular Material Added

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule
+  ]
+})
+export class MaterialModule { }
