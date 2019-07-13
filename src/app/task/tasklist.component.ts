c5aa447ded359e3b845f18660dd710fd5bb11b5d
Piotr Zysk <pizy123@gmail.com>
2019-07-13 10:44:42 +0200

    event.stopPropagation

--- src/app/task/tasklist.component.ts
+++ src/app/task/tasklist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-tasklist',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasklist.component.sass']
 })
 export class TasklistComponent implements OnInit {
+  tasks = [];
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.tasks = this.dataService.loadAllTasks();
   }
+
+  // stop propagating click event to paren component
+  onEvent(event) {
+    event.stopPropagation();
+ }
 
 }
