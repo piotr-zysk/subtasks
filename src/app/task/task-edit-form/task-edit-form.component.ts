76d52dc200dd22c5af098265616759a7e772503c
Piotr Zysk <pizy123@gmail.com>
2019-07-17 15:01:47 +0200

    Added: ReactiveForm base

--- src/app/task/task-edit-form/task-edit-form.component.ts
+++ src/app/task/task-edit-form/task-edit-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../models/task';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-task-edit-form',
@@ -11,13 +12,22 @@ export class TaskEditFormComponent implements OnInit {
   @Input() task: Task;
   @Output() formClose: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  taskForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.taskForm = new FormGroup({
+      title: new FormControl(),
+      done: new FormControl()
+     });
   }
 
   onCancel() {
     this.formClose.emit(false);
   }
 
+  save() {
+    this.formClose.emit(false);
+  }
 }
